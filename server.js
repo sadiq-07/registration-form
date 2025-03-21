3699830de98831bc4057c4bf3a0dc052c98bcd9c
sadiq-07 <msphubli@gmail.com>
2025-03-20 21:54:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
